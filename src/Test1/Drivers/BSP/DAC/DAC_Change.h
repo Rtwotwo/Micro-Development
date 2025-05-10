e46c77bae5d81c7f820d21192316b24fe91c829f
Rtwotwo <yoloredal@gmail.com>
2025-05-10 15:00:28 +0800

    Fixed folder's Name

--- src/Test1/Drivers/BSP/DAC/DAC_Change.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DAC_CHANGE_H
-#define __DAC_CHANGE_H
-
-void Wave_data_Init(void);
-void Start_DAC_DMA(uint8_t Num);
-void Stop_DAC_DMA(void);
-void Correct_DAC_Wave(uint8_t Num);
-#endif
-
--- src/Test1/Drivers/SYSTEM/delay/delay.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DELAY_H
-#define __DELAY_H
-
-void delay_ms(uint16_t nms);                /* —” ±nms */
-void delay_us(uint32_t nus);                /* —” ±nus */
-void delay_s(uint16_t ns);                  /* —” ±ns */
-#endif
-
